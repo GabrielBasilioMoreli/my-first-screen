d316e13c530b18cd5ed9fbce26c95fb4a3258d64
GabrielBasilioMoreli <gabrielbasilio1924@gmail.co>
2024-09-30 22:15:29 -0300

    adicionando text, textInput e touchebleoperaty

--- App.tsx
+++ App.tsx
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Digite seu nome</Text>
+      <TextInput placeholder='Nome'style={styles.textinp} />
+      <Text>Digite seu email</Text>
+      <TextInput placeholder='Email'style={styles.emailinp}/>
+      <Text>Digite sua senha</Text>
+      <TextInput placeholder='Senha'style={styles.senhainp} />
+      <TouchableOpacity activeOpacity ={0.5} style={styles.button}>
+        <Text>ENVIAR</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +21,45 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#808080',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  textinp: {
+    backgroundColor: '#FF7F50',
+    color: 'red',
+    width: 150,
+    height: 50,
+    borderRadius: 10,
+  },
+   
+  emailinp: {
+    backgroundColor: '#FF7F50',
+    color: 'red',
+    width: 150,
+    height: 50,
+    borderRadius: 10,
+  },
+
+  senhainp: {
+    backgroundColor: '#FF7F50',
+    color: 'red',
+    width: 150,
+    height: 50,
+    borderRadius: 10,
+  },
+  button: {
+    backgroundColor: 'rgb(192,192,192)',
+    padding: 15,
+    borderRadius: 7,
+    alignItems: 'center',
+    marginTop: 20,
+    width: 300,
+  },
+
+
+
+ 
+
+  },
+);
